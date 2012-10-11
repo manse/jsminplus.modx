c7f4a6ed994a2a31e82ed364999b53ecda12b067
manse <manse@www3352uf.sakura.ne.jp>
2012-10-11 18:24:13 +0900

    first commit

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+if (is_file(MODX_BASE_PATH . $plugin_base . 'JSMinPlus.php') && $modx->documentObject['contentType'] === 'text/javascript' && $modx->documentObject['cacheable']) {
+
+	require_once MODX_BASE_PATH . $plugin_base . 'JSMinPlus.php';
+	
+	$content = JSMinPlus::minify($content)
+	$modx->documentObject['content'] = $content;
+}
